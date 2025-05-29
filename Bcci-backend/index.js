53c401d228da98d894bb35c58682b28042da446d
jaiwanth97 <jaiwanth97@gmail.com>
2025-05-29 23:22:12 +0530

    Changes

--- Bcci-backend/index.js
+++ Bcci-backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
+ 
 // MongoDB Connection
 mongoose.connect('mongodb://localhost:27017/cricket_roster', {
     useNewUrlParser: true,
